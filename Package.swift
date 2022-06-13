dfd67ea57936ec7b5f21782b01ac721e1123fbb2
Jeff Johnston <hishma@users.noreply.github.com>
2022-06-13 08:05:26 -0700

    Update swift package manager.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            from: "0.3.0"),
+            from: "1.0.0"),
     ],
     targets: [
         .target(
